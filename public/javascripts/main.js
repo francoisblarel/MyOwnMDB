f4a339413be730838806f87171097f35327df611
François Blarel <francois.blarel@gmail.com>
2013-06-26 16:15:49 +0200

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    $('a[rel=tipsy]').tipsy({fade: true, gravity: 'n'});
+
     $("#charge").click(function(){
         window.location=Router.controllers.Application.charge().url
     })
